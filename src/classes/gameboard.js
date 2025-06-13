954fe6406cba5ea4c62f2e1930d789327f271a23
Ryan-D-Clark <Ryan@clarkonline.me.uk>
2025-06-13 03:44:44 +0100

    RC: Working in gameboard

--- src/classes/gameboard.js
+++ src/classes/gameboard.js
@@ -1,5 +1,51 @@
+const Ship = require("../classes/ship")
+
 class Gameboard{
     constructor(){
-        
+        this.ships = []
+        this.board = []
+        for(let x = 0; x < 100; x++){
+            this.board.push({shipLocation:false,isHit:false})
+        }
+
+        let carrier = new Ship(5,0,false)
+        let battleship = new Ship(4,0,false)
+        let cruiser = new Ship(3, 0, false)
+        let submarine = new Ship(2,0,false)
+        let destroyer = new Ship(4,0,false)
+        this.ships.push(carrier, battleship, cruiser, submarine, destroyer)
     }
+
+    getBoard(){
+        return this.board
+    }
+
+    getPosition(index){
+        return this.board[index]
+    }
+
+    receiveHit(index){
+        this.board[index].isHit = true
+    }
+
+    verifyHit(index){
+        return this.board[index].shipLocation
+    }
+
+    checkValidity(startingIndex, ship){
+        let invalidCells = [9,19,29,39,49,59,69,79,89,99]
+        // find modulus that takes it under 10, if adding ship length goes over 9 then it is invalid position
+
+    }
+
+    placeShip(startingIndex, ship){
+        console.log(this.board[startingIndex].shipLocation)
+    }
+
+    automaticPlacement(){
+
+    }
+
 }
+
+module.exports = Gameboard
--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -1,11 +1,37 @@
 const Gameboard = require("../src/classes/gameboard")
 
 describe("Testing functionality of gameboard", () =>{
-    let gameboard;
+    let gameboard
     beforeEach(() =>{
         gameboard = new Gameboard()
     })
-    test("Expects a 10x10 array to be returned", () =>{
-        expect()
+    test("Expects an array to have 100 items in it", () =>{
+        expect(gameboard.getBoard().length).toBe(100)
+    })
+    test("Expects a specifed index to return '{shipLocation:'false',isHit:'false'}'", () =>{
+        expect(gameboard.getPosition(10)).toStrictEqual({shipLocation:false,isHit:false})
+    })
+})
+
+describe("Testing creating and placing ships", () =>{
+    let gameboard
+    beforeEach(() =>{
+        gameboard = new Gameboard()
+    })
+    test("Expects the gameboard to return a list of ships", () =>{
+        expect(gameboard.ships.length).toBe(5)
+    })
+    test("Gameboard has function to change cell position status (when placing ships and receiving hits)", () =>{
+        gameboard.receiveHit(0)
+        expect(gameboard.getPosition(0)).toStrictEqual({shipLocation:false,isHit:true})
+    })
+    test("Checking validity by stopping a ship be placed at cell 9 horizontally", () =>{
+        console.log("TESTING", gameboard.checkValidity())
+        expect(gameboard.checkValidity(startingIndex, ship)).toBe(false)
+    })
+    test("Gameboard needs to be able to place a ship based on index and the following cells based on length", () =>{
+        console.log(gameboard.board[0].shipLocation)
+        gameboard.placeShip(0,gameboard.ships[0])
+        expect(gameboard.board[0].shipLocation  && gameboard.board[4].shipLocation).toBe(true)
     })
 })
